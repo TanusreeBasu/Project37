dbd49552ef5aec675efc5eea9355b452db51315f
TanusreeBasu <71438615+TanusreeBasu@users.noreply.github.com>
2021-04-02 13:43:23 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,88 @@
+class Quiz {
+  constructor(){
+    
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+   question.title.hide();
+   // question.question.hide();
+    question.input1.hide();
+    question.button.hide();
+    question.input2.hide();
+   //question.option1.hide();
+   // question.option2.hide();
+  // question.option3.hide();
+   //  question.option4.hide();
+//  question.hide();
+  
+    //write code to change the background color here
+    background("yellow");
+
+    //write code to show a heading for showing the result of Quiz
+    textSize(30);
+    text("RESULT OF THE QUIZ", 250,50);
+
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants !== undefined){
+      fill("blue");
+      textSize(15);
+      text("*NOTE : CONTESTANT WHO ANSWERED CORRECT ARE HIGHLIGHTED IN GREEN COLOUR",50,230)
+    //  var index =0 ;
+
+      //quiz.play();
+      for(var plr in allContestants){
+        // index = index +1
+        var correctAns = "2";
+       
+      //write code to add a note here
+  
+    //write code to highlight contest who answered correctly
+    if(correctAns ===allContestants[plr].answer){
+      fill("green");
+     textSize(20) 
+   strokeWeight(4)
+      text(allContestants[plr].name + ": "+ allContestants[plr].answer,100,300)
+    }
+    else{
+      fill("red");
+     textSize(20)
+     strokeWeight(4)
+    text(allContestants[plr].name + ": "+ allContestants[plr].answer,100,350)
+    }
+
+    }
+  }
+  }
+}
